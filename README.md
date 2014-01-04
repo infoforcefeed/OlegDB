1cc036be7dc993786e2cb75846837897e269584b
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-01-03 19:58:45 -0800

    Wooooo

--- README.md
+++ README.md
@@ -7,7 +7,11 @@ This is a shitty database written in C. Probably.
 
 Be terrible.
 
+Dependencies
+============
 
+* Message Pack
+* A healthy fear of the end
 
 Roadmap
 =======
@@ -15,16 +19,24 @@ Roadmap
 0.1
 ---
 
-* Store json
+* Volatile in memory messagepack
 * ACID Compliance
-* RAR storage for minimal storage
 * 16 byte hard field limit
 * Single-digit concurrency
 * Discardable soft mutexes
 * Non-guaranteed record expiry
 * Silent sharding
+* Pre-computed hashes
+
+0.2
+---
+* RAR storage for minimal storage
 
 1.0
 ---
 
 * WINRAR and WINE
+
+2.0
+---
+* BSP Export
---
+++ src/oleg.h
@@ -0,0 +1,33 @@
+//        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//                    Version 2, December 2004
+//
+// Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+//
+// Everyone is permitted to copy and distribute verbatim or modified
+// copies of this license document, and changing it is allowed as long
+// as the name is changed.
+//
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+//
+//  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+#include <msgpack.h>
+
+typedef enum {
+    OL_CONSUME_DIR      = 1 << 0,
+    OL_SLAUGHTER_DIR    = 1 << 1,
+    OL_CARRESS_DIR      = 1 << 2
+} ol_filemode;
+
+typedef msgpack_sbuffer *ol_obj;
+
+struct ol_database {
+    char name[8];                   // Name of the database
+    char path[256];                 // Path to the database directory
+    int  rcrd_cnt;                  // Number of records in the database. Eventually consistent.
+    ol_obj records[];     // All of the records in the database
+};
+
+ol_database *ol_open(char *path, ol_filemode filemode);
+ol_obj ol_get(char *path, ol_filemode filemode);
