a7dbb8d36f60744006cec0c1f7667c7e64c107c0
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-01-19 21:24:41 -0800

    Request handler echos back some stuff.

--- src/olegdb.erl
+++ src/olegdb.erl
@@ -1,23 +1,40 @@
 -module(olegdb).
--compile(export_all).
+-export([main/0]).
 
 -define(LISTEN_PORT, 8080).
 
 server_manager(Port) ->
     case gen_tcp:listen(Port, [list, {active, false}, {reuseaddr, true}]) of
         {ok, Sock} ->
+            io:format("[-] Listening on port ~p~n", [?LISTEN_PORT]),
             do_accept(Sock);
         {error, Reason} ->
             io:format("[X] Could not listen: ~p~n", [Reason])
     end.
 
+%% Responsible for accepting new connections and spawning request handlers.
 do_accept(Sock) ->
     case gen_tcp:accept(Sock) of
-        {ok, _} ->
+        {ok, Accepted} ->
             io:format("[-] Connection accepted!~n"),
+            spawn(fun() -> request_handler(Accepted) end),
             do_accept(Sock);
         {error, _} ->
             io:format("[X] Could not accept a connection.~n")
+    end.
+
+request_handler(Accepted) ->
+    % Read in all data, timeout after 60 seconds
+    case gen_tcp:recv(Accepted, 0, 60000) of
+        {ok, Data} ->
+            gen_tcp:send(Accepted, Data),
+            request_handler(Accepted);
+        {error, closed} ->
+            ok;
+        {error, timeout} ->
+            io:format("[-] Client timed out.~n"),
+            ok
+
     end.
 
 main() ->
