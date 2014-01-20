0b8ea492dc5744b58fd076cfde14f5c5c8979194
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-01-20 00:05:51 -0800

    Generic 404 response until we actually have data to give back.

--- src/olegdb.erl
+++ src/olegdb.erl
@@ -58,16 +58,26 @@ request_handler(Accepted) ->
             ok
     end.
 
+not_found_response() ->
+    "HTTP/1.1 404 Not Found\r\n"
+    "Status: 404 Not Found\r\n"
+    "Server: OlegDB/fresh_cuts_n_jams\r\n"
+    "Content-Length: 26\r\n"
+    "Connection: close\r\n"
+    "Content-Type: text/plain\r\n"
+    "\r\n"
+    "These aren't your ghosts.\n".
+
 route(Bits) -> 
     case Bits of
         <<"GET", _/binary>> ->
             io:format("[-] Header: ~p~n", [parsing:parse_get(Bits)]),
-            "Get request.";
+            not_found_response();
         <<"POST", _/binary>> ->
             io:format("[-] Header: ~p~n", [parsing:parse_post(Bits)]),
-            "Post request";
+            not_found_response();
         _ ->
-            "These aren't your ghosts."
+            not_found_response()
     end.
 
 main() ->
