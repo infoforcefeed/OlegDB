349c237577a29680881c3afbaeb625d233cb0e07
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-02-28 22:49:09 -0800

    Fixes binary data truncation bug. Fixes #34.

--- include/aol.h
+++ include/aol.h
@@ -27,4 +27,4 @@
 int ol_aol_init(ol_database *db);
 int ol_aol_write_cmd(ol_database *db, const char *cmd, ol_bucket *bucket);
 int ol_aol_restore(ol_database *db);
-int ol_aol_fsync(int fd);
+int ol_aol_fsync(FILE *fd);
--- integrity_test/large_file.py
+++ integrity_test/large_file.py
@@ -5,9 +5,13 @@ import sys
 def main(args):
     opened = open(args[1], "rb")
     url = "http://localhost:8080/{}".format(args[1])
-    requests.post(url, data=opened, headers={"Content-Type": args[2]})
+    if len(args) != 3:
+        requests.post(url, data=opened)
+        print "Posted to {}.".format(url)
+    else:
+        requests.post(url, data=opened, headers={"Content-Type": args[2]})
+        print "Posted to {} with content type {}".format(url, args[2])
     opened.close()
-    print "Posted to {} with content type {}".format(url, args[2])
 
 if __name__ == "__main__":
     main(sys.argv)
