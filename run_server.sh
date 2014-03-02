47dfc92a885ad1c7c545120382be723c7d0ca953
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-03-02 00:14:03 -0800

    Enable flags to actually use multiple processors. Oops.

--- run_server.sh
+++ run_server.sh
@@ -2,4 +2,4 @@
 
 export PATH=./build/bin/:$PATH
 export LD_LIBRARY_PATH=./build/lib/:$LD_LIBRARY_PATH
-erl -pa ./build/bin -noshell -s olegdb main -s init stop
+erl +K true -smp enable -pa ./build/bin -noshell -s olegdb main -s init stop
