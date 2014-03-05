5111f133a48d709f228c87f2514a502d091cc3f9
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-03-05 11:43:25 -0800

    Change the default maximum ports to 4096.

--- run_server.sh
+++ run_server.sh
@@ -2,4 +2,5 @@
 
 export PATH=./build/bin/:$PATH
 export LD_LIBRARY_PATH=./build/lib/:$LD_LIBRARY_PATH
+export ERL_MAX_PORTS=4096
 exec erl +K true -smp enable -pa ./build/bin -noshell -run olegdb main ${1+"$@"}
