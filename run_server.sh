6cc5649ef45595a6f3c359960e14b7344d80bde8
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-03-06 14:38:57 -0800

    Remove noshell, added noinput

--- run_server.sh
+++ run_server.sh
@@ -3,4 +3,4 @@
 export PATH=./build/bin/:$PATH
 export LD_LIBRARY_PATH=./build/lib/:$LD_LIBRARY_PATH
 export ERL_MAX_PORTS=4096
-exec erl +K true -smp enable -pa ./build/bin -noshell -run olegdb main ${1+"$@"}
+exec erl -noinput +K true -smp enable -pa ./build/bin -run olegdb main ${1+"$@"}
