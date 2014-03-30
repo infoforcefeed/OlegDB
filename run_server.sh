9e637b0b93914c69ada9258f7381406abf57fc66
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-03-30 13:54:54 -0700

    Bashery and hackery, swords and sorcery my sick scripts are legendary
    - Basename (No more dumb regex!)
    - more init improvements

--- run_server.sh
+++ run_server.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+RUN_DIR=/run/olegdb
+PID_FILE=$RUN_DIR/pid
+
 export ERL_MAX_PORTS=4096
-CMD_NAME=$(echo $0|grep -o '[a-zA-Z0-9]\+')
+CMD_NAME=`basename $0`
 if [ "$CMD_NAME" == 'olegdb' ]
     then
+        echo $$ > $PID_FILE
         exec erl -noinput +K true -smp enable -run olegdb main ${1+"$@"}
 fi
 export PATH=./build/bin/:$PATH
