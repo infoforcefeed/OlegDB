02d6fb57aaed40253508f9c0b9031d33f0b5e43b
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-04-02 23:11:15 -0700

    Added general purpose fuzzing tester. Manages to segfault after about 10 seconds. Good job, team.

---
+++ integrity_test/torture.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import json, calendar, requests, time, urllib, os, random, zlib
+
+def main():
+    while True:
+        random_length = random.random() * 10000
+        random_str = os.urandom(int(random_length))
+        compressed = zlib.compress(random_str)
+
+        random_key_str = os.urandom(10)
+        quoted = urllib.quote(random_key_str)
+        expiration = int(calendar.timegm(time.gmtime()) + (random.random() * 10))
+
+        connection_str = "http://localhost:8080/{}".format(quoted)
+        requests.post(connection_str,
+            data=compressed,
+            headers={
+                "Content-Type": "text/html",
+                "X-OlegDB-use-by": expiration})
+        resp = requests.get(connection_str, stream=True)
+        raw = resp.raw.read()
+        try:
+            assert(raw == compressed)
+        except AssertionError as e:
+            print "Assertion error: {} does not equal {}".format(hash(raw), hash(compressed))
+            print "Raw: {}".format(raw)
+            print "Response status: {}".format(resp.status_code)
+
+if __name__ == '__main__':
+    main()
