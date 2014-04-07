7366983adee380fa5db7d58b03850e7ea55deccf
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-04-06 23:12:00 -0700

    Removed original_bucket because gcc panics like a highschooler with an anxiety disorder.

--- src/oleg.c
+++ src/oleg.c
@@ -230,9 +230,8 @@ ol_bucket *_ol_get_bucket(const ol_database *db, const uint32_t hash, const char
     int index = _ol_calc_idx(db->cur_ht_size, hash);
     size_t larger_key = 0;
     if (db->hashes[index] != NULL) {
-        ol_bucket *tmp_bucket, *original_bucket;
-        /* original_bucket is only here for debugging purposes. */
-        original_bucket = tmp_bucket = db->hashes[index];
+        ol_bucket *tmp_bucket;
+        tmp_bucket = db->hashes[index];
         larger_key = tmp_bucket->klen > klen ? tmp_bucket->klen : klen;
         if (strncmp(tmp_bucket->key, key, larger_key) == 0) {
             return tmp_bucket;
