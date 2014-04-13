c1bd6e55b7129b5556c0ca2245cf44246e3f16d2
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-04-12 22:55:16 -0700

    Stubbed out the splay tree stuff.

--- Makefile
+++ Makefile
@@ -51,8 +51,8 @@ $(BIN_DIR)oleg_test: test.o main.o
 	$(libcc) $(CFLAGS) $(INCLUDES) -L$(LIB_DIR) -o $(BIN_DIR)oleg_test test.o main.o $(MATH_LINKER) -loleg
 
 liboleg: $(LIB_DIR)liboleg.so
-$(LIB_DIR)liboleg.so: murmur3.o oleg.o dump.o logging.o aol.o port_driver.o rehash.o utils.o
-	$(libcc) $(CFLAGS) $(INCLUDES) -o $(LIB_DIR)liboleg.so murmur3.o logging.o dump.o aol.o oleg.o rehash.o utils.o -fpic -shared $(MATH_LINKER)
+$(LIB_DIR)liboleg.so: murmur3.o oleg.o dump.o logging.o aol.o port_driver.o rehash.o utils.o tree.o
+	$(libcc) $(CFLAGS) $(INCLUDES) -o $(LIB_DIR)liboleg.so murmur3.o logging.o dump.o aol.o oleg.o tree.o rehash.o utils.o -fpic -shared $(MATH_LINKER)
 	$(libcc) $(CFLAGS) $(INCLUDES) $(ERLLIBS) -L$(LIB_DIR) -o $(LIB_DIR)libolegserver.so port_driver.o -fpic -shared $(MATH_LINKER) -loleg -lei
 
 server: $(BIN_DIR)ol_database.beam $(BIN_DIR)ol_http.beam \
---
+++ include/tree.h
@@ -0,0 +1,43 @@
+#pragma once
+/* The MIT License (MIT)
+* 
+* Copyright (c) 2014 Quinlan Pfiffer, Kyle Terry
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+/* Leaves of the splay tree */
+typedef struct ol_splay_tree_node {
+    struct ol_splay_tree_node *left;
+    struct ol_splay_tree_node *right;
+    struct ol_splay_tree_node *parent;
+
+    ol_bucket *bucket;
+} ol_splay_tree_node;
+
+/* The actual splay tree object */
+typedef struct ol_splay_tree {
+    ol_splay_tree_node *root;
+} ol_splay_tree;
+
+/* Insert and splay the tree */
+int ol_splay_insert(const ol_splay_tree *tree, ol_bucket *bucket);
+/* Delete and splay the tree */
+int ol_splay_delete(const ol_splay_tree *tree, ol_bucket *bucket);
+ol_bucket *ol_splay_find(const ol_splay_tree *tree);
