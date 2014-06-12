545e8ac3fe1de106fd4d36477187fad11372eeb7
zeroZshadow <martijn_gerkes@hotmail.com>
2014-06-12 23:15:14 +0200

    Checking for scoop return values [1202987]

--- src/oleg.c
+++ src/oleg.c
@@ -305,7 +305,7 @@ int ol_unjar_ds(ol_database *db, const char *key, size_t klen, unsigned char **d
             return 0;
         } else {
             /* It's dead, get rid of it. */
-            ol_scoop(db, key, klen);
+            check(ol_scoop(db, key, klen) == 0, "Scoop failed");
         }
     }
 
@@ -630,12 +630,11 @@ char *ol_content_type(ol_database *db, const char *key, size_t klen) {
             return bucket->content_type;
         } else {
             /* It's dead, get rid of it. */
-            int res = ol_scoop(db, key, klen);
-            if (res > 0)
-                debug("Could not delete a bucket!");
+            check(ol_scoop(db, key, klen) == 0, "Could not delete a bucket!")
         }
     }
-
+    
+error:
     return NULL;
 }
 
@@ -653,12 +652,11 @@ struct tm *ol_expiration_time(ol_database *db, const char *key, size_t klen) {
             return bucket->expiration;
         } else {
             /* It's dead, get rid of it. */
-            int res = ol_scoop(db, key, klen);
-            if (res > 0)
-                debug("Could not delete a bucket!");
+            check(ol_scoop(db, key, klen) == 0, "Could not delete a bucket!")
         }
     }
 
+error:
     return NULL;
 }
 
