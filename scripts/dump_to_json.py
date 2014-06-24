7d34fc4a5fec1ef7e6fb8674526dadcd5b3a621c
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-06-24 14:32:36 -0700

    Fixed a bunch of stuff. Decompresses LZ4. Expires records.

--- scripts/dump_to_json.py
+++ scripts/dump_to_json.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-import simplejson as json, sys
+import simplejson as json, sys, lz4
+from datetime import datetime
 
 class FuckOffException(Exception):
     pass
@@ -8,20 +9,20 @@ def read_command(opened_file):
     # :3:JAR:21:10138merveill.es_root:9:text/html:8:00021800:4230:<random data here>
     # Read in first colon
     _ = opened_file.read(1)
-    char = ""
-    length = ""
-    while char != None and char != ':':
+    char = b""
+    length = b""
+    while char != None and char != b':':
         char = opened_file.read(1)
         if not char:
             raise FuckOffException()
-        if char != ':':
+        if char != b':':
             length = length + char
 
     cmd = opened_file.read(int(length))
     if not cmd:
         raise FuckOffException()
 
-    return (length, unicode(cmd, errors='replace'))
+    return (length, cmd)
 
 def main(argv):
     if len(argv) < 2:
@@ -29,7 +30,7 @@ def main(argv):
         return 1
     database = {}
     print "Opening {}".format(argv[1])
-    v011_aol = open(argv[1], "r")
+    v011_aol = open(argv[1], "rb")
     while(1):
         try:
             clen, cmd = read_command(v011_aol)
@@ -37,13 +38,32 @@ def main(argv):
             if cmd == 'JAR':
                 ctlen, ctype = read_command(v011_aol)
                 compressed_size, compressed = read_command(v011_aol)
-                datasize, data = read_command(v011_aol)
-                database[key] = data
+                _ = v011_aol.read(1)
+                char = b""
+                length = b""
+                while char != None and char != b':':
+                    char = v011_aol.read(1)
+                    if not char:
+                        raise FuckOffException()
+                    if char != b':':
+                        length = length + char
+                database[key] = unicode(lz4.dumps(v011_aol.read(int(length))), errors="ignore")
             elif cmd == 'SPOIL':
                 # :5:SPOIL:30:10391merveill.espage%3D15_root:20:2014-04-28T12:44:12Z
                 datesize, date = read_command(v011_aol)
+                expired = datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
+
+                if expired <= datetime.now():
+                    try:
+                        del database[key]
+                    except KeyError:
+                        pass
+
             elif cmd == 'SCOOP':
-                "Doesn't matter."
+                try:
+                    del database[key]
+                except KeyError:
+                    pass
 
             v011_aol.read(1) # Newline.
         except FuckOffException:
@@ -53,7 +73,7 @@ def main(argv):
 
     output = argv[1] + ".json"
     opened_output = open(output, "w+")
-    json.dump(database, opened_output, encoding='utf-8', separators=(',', ':'))
+    json.dump(database, opened_output, separators=(',', ':'))
     opened_output.close()
 
     return 0
