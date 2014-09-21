fda97c568586825b72548211d48038ac58292676
Hamcha <zikyky@gmail.com>
2014-09-21 02:45:24 +0200

    Big refactor of Golegd to be better organized

---
+++ frontend/operations.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const (
+	OpGet         = "/get"
+	OpSet         = "/post"
+	OpInfo        = "._info"
+	OpDelete      = "/delete"
+	OpCursorFirst = "_first"
+	OpCursorLast  = "_last"
+	OpCursorNext  = "._next"
+	OpCursorPrev  = "._prev"
+	OpPrefixMatch = "_match"
+)
+
+func httpGet(w http.ResponseWriter, op Operation) *HTTPError {
+	value := op.Database.Unjar(op.Key)
+	// Check if the item existed
+	if value == nil {
+		return &HTTPError{Code: 404, Message: "Key not found in database"}
+	}
+
+	// Send value
+	fmt.Fprintf(w, string(value))
+	return nil
+}
+
+func httpSet(w http.ResponseWriter, op Operation, r *http.Request) *HTTPError {
+	value, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return &HTTPError{Code: 500, Message: "Your post body is messed up!"}
+	}
+
+	// Check if value already existed
+	exists := op.Database.Exists(op.Key)
+
+	res := op.Database.Jar(op.Key, value)
+	if res == 0 {
+		// Status 201 if created, 200 if updated
+		if exists {
+			w.WriteHeader(200)
+		} else {
+			w.WriteHeader(201)
+		}
+		fmt.Fprintf(w, "Value set successfully!")
+	} else {
+		return &HTTPError{Code: 500, Message: "Something went horribly wrong..."}
+	}
+
+	// Try to set expiration, if provided
+	if eep, ok := r.Header["X-Olegdb-Use-By"]; ok {
+		ep, err := strconv.Atoi(eep[0])
+		if err != nil {
+			return &HTTPError{Code: 500, Message: "The expiration format is wrong!"}
+		}
+		date := time.Unix(int64(ep), 0)
+		op.Database.Spoil(op.Key, date)
+		fmt.Fprintf(w, "\r\nThe jar is spoiling!")
+	}
+
+	return nil
+}
+
+func httpInfo(w http.ResponseWriter, op Operation) *HTTPError {
+	// Does it even exists?
+	if !op.Database.Exists(op.Key) {
+		return &HTTPError{Code: 404, Message: "Key not found in database"}
+	}
+
+	// Get and set Expiration
+	res, doesExpire := op.Database.Expiration(op.Key)
+	if doesExpire {
+		w.Header().Add("Expires", strconv.Itoa(int(res.UTC().Unix())))
+	}
+
+	// Add Record count
+	w.Header().Add("X-Olegdb-Rcrd-Cnt", strconv.Itoa(int(*op.Database.RecordCount)))
+
+	// Send empty body
+	fmt.Fprintf(w, "\r\n")
+	return nil
+}
+
+func httpDelete(w http.ResponseWriter, op Operation) *HTTPError {
+	res := op.Database.Scoop(op.Key)
+	if res != 0 {
+		return &HTTPError{Code: 500, Message: "Something went horribly wrong..."}
+	}
+
+	fmt.Fprintf(w, "Key deleted successfully!")
+	return nil
+}
