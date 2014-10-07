a603f16629be88d517b9a8c91ef8c0d64995acb3
Hamcha <zikyky@gmail.com>
2014-10-07 07:01:16 +0200

    Couple more tests

--- frontend/goleg/goleg_test.go
+++ frontend/goleg/goleg_test.go
@@ -3,16 +3,18 @@ package goleg
 import (
 	"io/ioutil"
 	"os"
+	"strconv"
 	"testing"
 )
 
-func openRandomDB() (Database, string, error) {
+func openRandomDB(features int) (Database, string, error) {
 	name, err := ioutil.TempDir("/tmp", "goleg")
 	if err != nil {
 		return Database{}, "", err
 	}
 
-	database, err := Open(name, "test", F_APPENDONLY|F_AOL_FFLUSH|F_LZ4|F_SPLAYTREE)
+	//F_APPENDONLY|F_AOL_FFLUSH|F_LZ4|F_SPLAYTREE
+	database, err := Open(name, "test", features)
 	if err != nil {
 		return Database{}, "", err
 	}
@@ -32,6 +34,51 @@ func TestOpen(t *testing.T) {
 	database, dir, err := openRandomDB()
 	if err != nil {
 		t.Fatalf("Can't open database: %s", err.Error())
+	}
+
+	database.Close()
+	cleanTemp(dir)
+}
+
+const JARN = 1000
+
+func TestJar(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping in short mode")
+	}
+
+	database, dir, err := openRandomDB(F_LZ4 | F_SPLAYTREE)
+	if err != nil {
+		t.Fatalf("Can't open database: %s", err.Error())
+	}
+
+	for i := 0; i < JARN; i++ {
+		if !database.Jar("record"+strconv.Itoa(i), []byte("value"+strconv.Itoa(i))) {
+			t.Fatalf("Can't jar value #%d", i)
+		}
+	}
+
+	database.Close()
+	cleanTemp(dir)
+}
+
+func TestUnjar(t *testing.T) {
+	database, dir, err := openRandomDB(F_LZ4 | F_SPLAYTREE)
+	if err != nil {
+		t.Fatalf("Can't open database: %s", err.Error())
+	}
+
+	for i := 0; i < JARN; i++ {
+		if !database.Jar("record"+strconv.Itoa(i), []byte("value"+strconv.Itoa(i))) {
+			t.Fatalf("Can't jar value #%d", i)
+		}
+	}
+
+	for i := 0; i < JARN; i++ {
+		val := database.Unjar("record" + strconv.Itoa(i))
+		if val != []byte("value"+strconv.Itoa(i)) {
+			t.Errorf("Value #%d doesn't match", i)
+		}
 	}
 
 	database.Close()
