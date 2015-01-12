5750928e7bc6c4b3be726f4669100b0d06239150
Kyle Terry <kyle@kyleterry.com>
2015-01-11 20:11:30 -0800

    Added test for full key dump

--- frontend/goleg/goleg_test.go
+++ frontend/goleg/goleg_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"testing"
+	"fmt"
 )
 
 func openRandomDB(features int) (Database, string, error) {
@@ -84,4 +85,37 @@ func TestUnjar(t *testing.T) {
 
 	database.Close()
 	cleanTemp(dir)
+}
+
+func TestFullKeyDump(t *testing.T) {
+	database, _, err := openRandomDB(F_LZ4 | F_SPLAYTREE)
+	if err != nil {
+		t.Fatalf("Can't open database: %s", err.Error())
+	}
+
+	for i := 0; i < JARN; i++ {
+		if database.Jar("record"+strconv.Itoa(i), []byte("value"+strconv.Itoa(i))) != 0 {
+			t.Fatalf("Can't jar value #%d", i)
+		}
+	}
+
+	gotKeys, keys := database.DumpKeys()
+	
+	if !gotKeys {
+		t.Fatal("Didn't get keys and should have")
+	}
+
+	fmt.Println(keys)
+
+	var j int
+	for i := 0; i <= JARN; i++ {
+		for _, key := range keys {
+			if key == "record"+strconv.Itoa(i) {
+				j++
+			}
+		}
+	}
+	if j != JARN {
+		t.Fatal("One or more keys did not dump")
+	}
 }
