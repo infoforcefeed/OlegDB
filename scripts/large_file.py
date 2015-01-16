b0c9b22ce00f525bb3ef54bc53c512d6dd2b317f
Quinlan Pfiffer <qpfiffer@gmail.com>
2015-01-15 17:49:25 -0800

    Update large_file script.

--- scripts/large_file.py
+++ scripts/large_file.py
@@ -4,13 +4,11 @@ import sys
 
 def main(args):
     opened = open(args[1], "rb")
-    url = "http://localhost:8080/oleg/{}".format(args[1])
-    if len(args) != 3:
-        requests.post(url, data=opened)
-        print "Posted to {}.".format(url)
-    else:
-        requests.post(url, data=opened, headers={"Content-Type": args[2]})
-        print "Posted to {} with content type {}".format(url, args[2])
+    url = "http://localhost:38080/oleg/{}".format(args[2])
+
+    requests.post(url, data=opened)
+    print "Posted to {}.".format(url)
+
     opened.close()
 
 if __name__ == "__main__":
